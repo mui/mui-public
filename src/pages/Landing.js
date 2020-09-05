2675a7bc2014cd67d71092fa8801eff346584dab
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-09-05 12:18:05 +0200

    feat: Add azure pipelines monitor (#13)

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -8,7 +8,7 @@ import Link from "@material-ui/core/Link";
 import Skeleton from "@material-ui/lab/Skeleton";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { usePaginatedQuery } from "react-query";
+import { usePaginatedQuery, useQuery } from "react-query";
 import styled from "styled-components";
 import ErrorBoundary from "../components/ErrorBoundary";
 import Heading from "../components/Heading";
@@ -23,6 +23,10 @@ export default function Landing() {
 				CircleCI workflows
 			</Heading>
 			<CircleCIWorkflows />
+			<Heading level="2" id="azure-pipelines-runs">
+				Azure Pipelines runs
+			</Heading>
+			<AzurePipeline />
 			<Heading level="2" id="webpagetests">
 				Webpagetests
 			</Heading>
@@ -31,6 +35,145 @@ export default function Landing() {
 			</React.Suspense>
 		</React.unstable_SuspenseList>
 	);
+}
+
+function AzurePipeline() {
+	const pipelineId = 1;
+	const buildGroups = [
+		{
+			name: "master",
+			label: "material-ui@master",
+			filter: { branchName: "refs/heads/master", reasonFilter: "individualCI" },
+		},
+		{
+			name: "next",
+			label: "material-ui@next",
+			filter: { branchName: "refs/heads/next", reasonFilter: "individualCI" },
+		},
+		{
+			name: "master-next",
+			label: "material-ui@master /w react@next",
+			filter: { branchName: "refs/heads/master", reasonFilter: "schedule" },
+		},
+		{
+			name: "next-next",
+			label: "material-ui@next /w react@next",
+			filter: { branchName: "refs/heads/next", reasonFilter: "schedule" },
+		},
+	];
+
+	return (
+		<React.unstable_SuspenseList revealOrder="forwards">
+			{buildGroups.map((buildGroup) => {
+				return (
+					<React.Suspense
+						fallback={<Skeleton height={48} variant="rect" />}
+						key={buildGroup.label}
+					>
+						<ErrorBoundary
+							fallback={
+								<p>Failed fetching CircleCI builds for {buildGroup.label}</p>
+							}
+						>
+							<AzurePipelineBuildGroup
+								pipelineId={pipelineId}
+								buildGroup={buildGroup}
+							/>
+						</ErrorBoundary>
+					</React.Suspense>
+				);
+			})}
+		</React.unstable_SuspenseList>
+	);
+}
+
+function AzurePipelineBuildGroup(props) {
+	const { pipelineId, buildGroup } = props;
+
+	const builds = useRecentAzurePipelinesBuilds({
+		definitions: pipelineId,
+		...buildGroup.filter,
+	});
+	const [lastBuild] = builds;
+
+	return (
+		<Accordion>
+			<AccordionSummary
+				aria-controls={`circleci-workflow-${buildGroup.name}-content`}
+				id={`circleci-workflow-${buildGroup.name}-header`}
+				expandIcon={<ExpandMoreIcon />}
+			>
+				<Typography>
+					{buildGroup.label}{" "}
+					<em>
+						{lastBuild === undefined ? "state unknown" : lastBuild.result}
+					</em>
+				</Typography>
+			</AccordionSummary>
+			<AccordionDetails>
+				<AzurePipelineBuilds builds={builds} />
+			</AccordionDetails>
+		</Accordion>
+	);
+}
+
+const AzurePipelineBuild = styled(ListItem)`
+	display: inline-block;
+`;
+
+function AzurePipelineBuilds(props) {
+	const { builds } = props;
+
+	return (
+		<List>
+			{builds.map((build) => {
+				return (
+					<AzurePipelineBuild key={build.id}>
+						<Link href={build._links.web.href}>
+							#{build.buildNumber}@
+							{build.sourceBranch.replace(/^refs\/heads\//, "")}
+						</Link>
+						{" finished "}
+						<RelativeTimeTillNow time={build.finishTime} />
+					</AzurePipelineBuild>
+				);
+			})}
+		</List>
+	);
+}
+
+function useRecentAzurePipelinesBuilds(filter) {
+	const { data: builds } = useQuery(
+		["azure-pipelines-builds", filter],
+		fetchRecentAzurePipelinesBuilds
+	);
+	React.useDebugValue(builds);
+
+	return React.useMemo(() => builds.slice(0, 20), [builds]);
+}
+
+async function fetchRecentAzurePipelinesBuilds(key, filter) {
+	const url = getAzurePipelinesAPIUrl(
+		"mui-org/material-ui/_apis/build/builds",
+		{
+			"api-version": "6.0",
+			...filter,
+		}
+	);
+	const response = await fetch(url);
+	const { value: builds } = await response.json();
+
+	return builds;
+}
+
+function getAzurePipelinesAPIUrl(endpoint, params) {
+	const apiEndpoint = "https://dev.azure.com/";
+	const url = new URL(`${apiEndpoint}${endpoint}`);
+	new URLSearchParams({
+		...params,
+	}).forEach((value, key) => url.searchParams.append(key, value));
+
+	return url;
 }
 
 function CircleCIWorkflows() {
@@ -174,6 +317,12 @@ function RelativeTimeTillNow(props) {
 	const now = new Date();
 	const then = new Date(props.time);
 	const seconds = (then - now) / 1000;
+	if (isNaN(then.getTime())) {
+		if (process.env.NODE_ENV !== "production") {
+			console.warn("Invalid Date given with %s", props.time);
+		}
+		return "Unknown";
+	}
 
 	const intl = new Intl.RelativeTimeFormat("en", { numeric: "always" });
 
