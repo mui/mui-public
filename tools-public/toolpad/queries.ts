c8e13d8f819ec95d70bb27e53377c405fe38ba21
prakhargupta1 <prakhar@mui.com>
2023-03-22 14:50:27 +0100

    HEAD CI success rate app
    https://www.notion.so/mui-org/KPIs-1ce9658b85ce4628a2a2ed2ae74ff69c?pvs=4#f36a86b0a0b14d028e744423d9bc6b44

--- tools-public/toolpad/queries.ts
+++ tools-public/toolpad/queries.ts
@@ -1,4 +1,5 @@
 import { createQuery } from "@mui/toolpad-core";
+import { request } from "graphql-request";
 
 export const getRepositoryDetails = createQuery(
   async function getRepositoryDetails({ parameters }) {
@@ -24,178 +25,6 @@ export const getRepositoryDetails = createQuery(
   }
 );
 
-/* TO BE REMOVED
-
-export const getContributorStats = createQuery(
-  async function getContributorStats({ parameters }) {
-    const openQuery = `
-with pr_open as (
-  SELECT
-    ge.actor_id,
-    ge.pr_or_issue_id,
-    ANY_VALUE(ge.actor_login) AS actor_login,
-    CONCAT(YEAR(ge.created_at), '-', MONTH(ge.created_at)) as month
-  FROM
-    github_events ge
-  WHERE
-    ge.repo_id = 23083156
-    AND ge.type = 'PullRequestEvent'
-    AND ge.action = 'opened'
-    AND ge.created_at >= '2022-11-01'
-    AND ge.actor_login NOT LIKE '%bot'
-    AND ge.actor_login NOT LIKE '%[bot]'
-), pr_open_group as (
-  SELECT
-    ge.actor_id,
-    ge.month,
-    ge.actor_login,
-    COUNT(DISTINCT ge.pr_or_issue_id) AS prs
-  FROM pr_open ge
-  GROUP BY
-    ge.actor_id, ge.month, ge.actor_login
-  ORDER BY
-    ge.month desc,
-    prs desc
-)
-SELECT * FROM pr_open_group ge WHERE ge.prs > 1;
-    `;
-    const res = await fetch("https://api.ossinsight.io/q/playground", {
-      "headers": {
-        "content-type": "application/json",
-      },
-      "body": JSON.stringify({sql: openQuery,"type":"repo","id":"23083156"}),
-      "method": "POST"
-    });
-    if (res.status !== 200) {
-      throw new Error(
-        `HTTP ${res.status}: ${(await res.text()).slice(0, 500)}`
-      );
-    }
-    const data = await res.json();
-    return data.data;
-  },
-  {
-    parameters: {
-      // orderIds: {
-      //   typeDef: { type: "string" },
-      // },
-    },
-  }
-);
-
-
-
-export const PRsOpenQuery = createQuery(
-  async function PRsOpenQuery ({ parameters }) {
-    const openQuery = `
-    SELECT
-    ge.actor_id, year(ge.created_at) as year, month(ge.created_at) as month, 
-    ANY_VALUE(ge.actor_login) AS actor_login,
-    COUNT(DISTINCT ge.pr_or_issue_id) AS prs_created
-  FROM
-    github_events ge
-  WHERE
-    ge.repo_id = 23083156
-    AND ge.type = 'PullRequestEvent'
-    AND ge.action = 'opened'
-    AND ge.created_at >= '2023-01-01'
-   -- AND ge.created_at < '2023-12-01'
-    AND ge.actor_login NOT LIKE '%bot'
-    AND ge.actor_login NOT LIKE '%[bot]'
-  GROUP BY
-    ge.actor_id, year, month
-  ORDER BY
-    year desc, month desc, prs_created desc;
-    `;
-    const res = await fetch("https://api.ossinsight.io/q/playground", {
-      "headers": {
-        "content-type": "application/json",
-      },
-      "body": JSON.stringify({sql: openQuery,"type":"repo","id":"23083156"}),
-      "method": "POST"
-    });
-    if (res.status !== 200) {
-      throw new Error(
-        `HTTP ${res.status}: ${(await res.text()).slice(0, 500)}`
-      );
-    }
-    const data = await res.json();
-    return data.data;
-  },
-  {
-    parameters: {
-      // orderIds: {
-      //   typeDef: { type: "string" },
-      // },
-    },
-  }
-);
-
-
-export const PRsReviewed = createQuery(
-  async function PRsReviewed ({ parameters }) {
-    const openQuery = `
-    SELECT
-  ge.actor_id, year(ge.created_at) as year, month(ge.created_at) as month,
-  ANY_VALUE(ge.actor_login) AS actor_login,
-  COUNT(DISTINCT ge.pr_or_issue_id) AS prs_reviewed
-FROM
-  github_events ge
-  -- join with all pull request created by someone outside of the Core team
-  JOIN (
-    SELECT
-      *
-    from
-      github_events ge2
-    where
-      ge2.repo_id = 23083156
-      AND ge2.type like "PullRequestEvent"
-      AND ge2.action = "opened"
-      AND ge2.actor_login NOT LIKE 'mnajdova'
-      AND ge2.actor_login NOT LIKE 'michaldudak'
-      AND ge2.actor_login NOT LIKE 'siriwatknp'
-      AND ge2.actor_login NOT LIKE 'hbjORbj'
-      AND ge2.actor_login NOT LIKE 'oliviertassinari'
-      AND ge2.actor_login NOT LIKE 'mj12albert'
-  ) as pr_creators ON ge.pr_or_issue_id = pr_creators.pr_or_issue_id
-  AND ge.actor_id <> pr_creators.actor_id
-WHERE
-  ge.repo_id = 23083156
-  AND ge.type = 'PullRequestReviewEvent'
-  AND ge.action = 'created'
-  AND ge.created_at >= '2023-01-01'
-  -- AND ge.created_at < '2023-02-01'
-  AND ge.actor_login NOT LIKE '%bot'
-  AND ge.actor_login NOT LIKE '%[bot]'
-GROUP BY
-  ge.actor_id, year, month
-ORDER BY
-  year desc, month desc, prs_reviewed desc;
-    `;
-    const res = await fetch("https://api.ossinsight.io/q/playground", {
-      "headers": {
-        "content-type": "application/json",
-      },
-      "body": JSON.stringify({sql: openQuery,"type":"repo","id":"23083156"}),
-      "method": "POST"
-    });
-    if (res.status !== 200) {
-      throw new Error(
-        `HTTP ${res.status}: ${(await res.text()).slice(0, 500)}`
-      );
-    }
-    const data = await res.json();
-    return data.data;
-  },
-  {
-    parameters: {
-      // orderIds: {
-      //   typeDef: { type: "string" },
-      // },
-    },
-  }
-);
-*/
 
 export const PRsOpenandReviewedQuery = createQuery(
   async function PRsOpenandReviewedQuery ({ parameters }) {
@@ -299,3 +128,56 @@ export const PRsOpenandReviewedQuery = createQuery(
   },
 }
 );
+
+
+export const queryCommitStatuses = createQuery(
+async function queryCommitStatuses({ parameters }) {
+
+if (!process.env.GITHUB_TOKEN) {
+    throw new Error(`Env variable GITHUB_TOKEN not configured`);
+}
+
+const since = new Date();
+since.setDate(since.getDate() - 7);
+
+const endpoint = 'https://api.github.com/graphql';
+const token = process.env.GITHUB_TOKEN;
+
+const query = `
+{
+  repository(owner: "mui", name: "${parameters.repository}") {
+  	defaultBranchRef {
+      id
+      name
+      target {
+        ... on Commit {
+          history(since: "${since.toISOString()}") {
+            nodes {
+              messageHeadline
+              committedDate
+              status {
+                state
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}  
+`;
+
+const response =  request(endpoint, query, null, {
+  Authorization: `Bearer ${token}`,
+})
+
+return response;
+
+},
+{
+  parameters: {
+      repository: {
+      typeDef: { type: "string" },
+     },
+  },
+})
