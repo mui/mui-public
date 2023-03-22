51d219fdd83f48b7989e77d5efa49aced53d55a0
prakhargupta1 <prakhar@mui.com>
2023-03-22 15:27:46 +0100

    Add overdue ratio app
    Adding overdue ratio app back in mui-public

--- tools-public/toolpad/queries.ts
+++ tools-public/toolpad/queries.ts
@@ -1,5 +1,8 @@
 import { createQuery } from "@mui/toolpad-core";
 import { request } from "graphql-request";
+import mysql from 'mysql2/promise';
+import SSH2Promise from 'ssh2-promise';
+
 
 export const getRepositoryDetails = createQuery(
   async function getRepositoryDetails({ parameters }) {
@@ -180,4 +183,104 @@ return response;
       typeDef: { type: "string" },
      },
   },
+})
+
+
+
+export const getRatio = createQuery(
+  async function getRatio({ parameters }) {
+    if (!process.env.STORE_PASSWORD) {
+      throw new Error(`Env variable STORE_PASSWORD not configured`);
+    }
+    if (!process.env.BASTION_SSH_KEY) {
+      throw new Error(`Env variable BASTION_SSH_KEY not configured`);
+    }
+
+    const ssh = new SSH2Promise({
+      host: process.env.BASTION_HOST,
+      port: 22,
+      username: process.env.BASTION_USERNAME ,
+      privateKey: process.env.BASTION_SSH_KEY.replace(/\\n/g, '\n'),
+    });
+
+    const tunnel = await ssh.addTunnel({
+      remoteAddr: 'c111501.sgvps.net',
+      remotePort: 3306,
+    })
+
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      port: tunnel.localPort,
+      user: process.env.STORE_USERNAME,
+      password: process.env.STORE_PASSWORD,
+      database: process.env.STORE_DATABASE,
+    });
+
+    const [ratio] = await connection.execute(`
+    SELECT
+    overdue.total / order_30.total AS ratio
+    -- overdue.total,
+    -- order_30.total
+  FROM
+    (
+      SELECT
+        sum(post.total) AS total
+      FROM
+        (
+          SELECT
+            postmeta1.meta_value - postmeta2.meta_value AS total,
+            CASE
+              WHEN postmeta3.meta_value IS NOT NULL THEN postmeta3.meta_value
+              ELSE 30
+            END AS payment_term,
+            from_unixtime(
+              postmeta4.meta_value + (
+                CASE
+                  WHEN postmeta3.meta_value IS NOT NULL THEN postmeta3.meta_value
+                  ELSE 30
+                END
+              ) * 3600 * 24
+            ) AS invoice_due,
+            postmeta4.meta_value AS invoice_date,
+            postmeta5.meta_value AS billing_country,
+            post.id
+          FROM
+            wp3u_posts post
+            LEFT JOIN wp3u_postmeta postmeta1 ON postmeta1.post_id = post.id
+            AND postmeta1.meta_key = '_order_total'
+            LEFT JOIN wp3u_postmeta postmeta2 ON postmeta2.post_id = post.id
+            AND postmeta2.meta_key = '_order_tax'
+            LEFT JOIN wp3u_postmeta postmeta3 ON postmeta3.post_id = post.id
+            AND postmeta3.meta_key = 'payment_term'
+            LEFT JOIN wp3u_postmeta postmeta4 ON postmeta4.post_id = post.id
+            AND postmeta4.meta_key = '_wcpdf_invoice_date'
+            LEFT JOIN wp3u_postmeta postmeta5 ON postmeta5.post_id = post.id
+            AND postmeta5.meta_key = '_billing_country'
+          WHERE
+            post.post_status = 'wc-processing'
+            AND post.post_type = 'shop_order'
+            AND post.post_parent = '0' -- ignore orders that are sub-orders
+          order by
+            post.post_date desc
+        ) AS post
+      WHERE
+        DATEDIFF(now(), post.invoice_due) > 0
+    ) AS overdue,
+    (
+      SELECT
+        sum(postmeta1.meta_value - postmeta2.meta_value) AS total
+      FROM
+        wp3u_posts post
+        LEFT JOIN wp3u_postmeta postmeta1 ON postmeta1.post_id = post.id
+        AND postmeta1.meta_key = '_order_total'
+        LEFT JOIN wp3u_postmeta postmeta2 ON postmeta2.post_id = post.id
+        AND postmeta2.meta_key = '_order_tax'
+      WHERE
+        post.post_status = 'wc-completed'
+        AND post.post_type = 'shop_order'
+        AND post.post_date >= date_sub(now(), interval 30 day)
+        AND post.post_parent = '0' -- ignore orders that are sub-orders
+    ) AS order_30
+  `)
+return ratio[0];
 })
