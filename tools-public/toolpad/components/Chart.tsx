9e039802fe10d60643dd9269a39e35213201914f
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2023-04-21 15:17:04 +0200

    viteRuntime

--- tools-public/toolpad/components/Chart.tsx
+++ tools-public/toolpad/components/Chart.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { createComponent } from "@mui/toolpad/browser";
 import {
+  ResponsiveContainer,
   LineChart,
   Line,
   XAxis,
@@ -8,7 +9,7 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
-} from "https://esm.sh/recharts@2.2.0";
+} from "recharts";
 
 // Copied from https://wpdatatables.com/data-visualization-color-palette/
 const COLORS = [
@@ -29,25 +30,27 @@ export interface PieChartProps {
 
 function ChartExport({ data }: PieChartProps) {
   return (
-    <LineChart
-      width={800}
-      height={300}
-      data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="event_month" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line type="monotone" dataKey="reviewed" stroke={COLORS[6]} />
-      <Line type="monotone" dataKey="opened" stroke={COLORS[7]} />
-    </LineChart>
+    <ResponsiveContainer width="100%" height={300}>
+      <LineChart
+        width={800}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="event_month" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="reviewed" stroke={COLORS[6]} />
+        <Line type="monotone" dataKey="opened" stroke={COLORS[7]} />
+      </LineChart>
+    </ResponsiveContainer>
   );
 }
 
