6acc4c0f84f8afae33ae454dfcf5f4f7277d2233
Marija Najdova <mnajdova@gmail.com>
2023-05-25 14:10:21 +0200

    [tools] Add new page for Base UI npm downloads KPIs (#102)

--- tools-public/toolpad/resources/functions.ts
+++ tools-public/toolpad/resources/functions.ts
@@ -291,3 +291,5 @@ export * from "./queryMUIXLabels";
 export * from "./queryPRs";
 export * from "./queryPRs2";
 export * from "./queryGender";
+export * from "./queryHeadlessLibrariesDownloads";
+export * from "./queryJoyUIMonthlyDownloads";
