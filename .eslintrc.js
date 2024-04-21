4eb8d700c2f65b58f9de1cd4e6fcd8bf317724a8
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-04-21 16:59:54 +0200

    [core] Remove dead eslint code

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,23 +19,9 @@ module.exports = {
     // No time for this
     'react/prop-types': 'off',
     'jsx-a11y/control-has-associated-label': 'off',
-    // No time for this
     'jsx-a11y/no-autofocus': 'off',
   },
   overrides: [
     ...baseline.overrides,
-    {
-      files: [
-        // matching the pattern of the test runner
-        'accounting/**',
-        'lambda/**',
-        'BI/**',
-        'web/**',
-      ],
-      rules: {
-        'no-console': 'off',
-        'import/no-unresolved': 'off',
-      },
-    },
   ],
 };
