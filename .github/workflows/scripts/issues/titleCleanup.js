b889079f7c1a08c6f667f6ee7eaebd2499a23c6e
michel <jsnerdic@gmail.com>
2024-12-13 12:41:02 +0100

    review remarks

--- .github/workflows/scripts/issues/titleCleanup.js
+++ .github/workflows/scripts/issues/titleCleanup.js
@@ -1,13 +1,13 @@
 // @ts-check
 const regexes = {
   // covers 'dataGrid' in all variants, as well as with suffixes (-pro/-premium/etc)
-  dataGrid: /\s*\[\s*(data\s?grid[-\sA-Za-z]*)]/gi,
+  dataGrid: /\s*\[\s*(data[\s-]?grid[-\sA-Za-z]*)\s*]/gi,
   // covers 'charts' in all variants, as well as with prefixes (PieChart, LineChart, etc)
-  charts: /\s*\[\s*([-\sA-Za-z]*charts?)]/gi,
+  charts: /\s*\[\s*([-\sA-Za-z]*charts?)\s*]/gi,
   // covers 'pickers' in all variants, as well as with prefixes (DatePicker, time-picker, etc)
-  pickers: /\s*\[\s*([-\sA-Za-z]*pickers?)]/gi,
+  pickers: /\s*\[\s*([-\sA-Za-z]*pickers?)\s*]/gi,
   // covers 'treeView' in all variants
-  treeView: /\s*\[\s*([-\sA-Za-z]*tree\s?(view|item))]/gi,
+  treeView: /\s*\[\s*([-\sA-Za-z]*tree\s?(view|item))\s*]/gi,
 };
 
 const replacements = {
@@ -16,9 +16,6 @@ const replacements = {
   pickers: 'pickers',
   treeView: 'tree view',
 };
-
-const regex =
-  /\[\s*(?:(?<dataGrid>data\s?grid.*)|(?<charts>.*charts?)|(?<pickers>.*pickers?)|(?<treeView>tree\s?(view|item)))\s*]/gi;
 
 /**
  * @param {Object} params
