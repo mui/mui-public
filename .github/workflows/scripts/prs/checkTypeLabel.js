c5d8b2dce81d2dbcc4f0a5cfd487cd697035aa4b
Michel Engelen <32863416+michelengelen@users.noreply.github.com>
2025-03-18 14:00:53 +0100

    [infra] Fix multiple comments issue (#285)

--- .github/workflows/scripts/prs/checkTypeLabel.js
+++ .github/workflows/scripts/prs/checkTypeLabel.js
@@ -21,6 +21,67 @@ const typeLabels = [
 ];
 const labelRegex = new RegExp(`\\b(${typeLabels.join('|')})\\b`, 'i');
 
+const COMMENT_FIRST_LINE = {
+  NO_LABELS_COMMENT:
+    'Please add one type label to categorize the purpose of this PR appropriately:',
+  MULTIPLE_LABELS_COMMENT: 'Multiple type labels found:',
+  SUCCESS_COMMENT: 'Thanks for adding a type label to the PR! 👍',
+};
+
+/**
+ * @param {Awaited<ReturnType<ReturnType<import("@actions/github").getOctokit>['rest']['issues']['listComments']>>['data']} comments
+ */
+function containsAny(comments) {
+  return (
+    comments.find((comment) =>
+      Object.values(COMMENT_FIRST_LINE).some((startLine) => comment.body?.startsWith(startLine)),
+    ) ?? false
+  );
+}
+
+/**
+ * @param {Object} params
+ * @param {import("@actions/core")} params.core
+ * @param {ReturnType<import("@actions/github").getOctokit>} params.github
+ * @param {import("@actions/github").context} params.context
+ * @param {Awaited<ReturnType<ReturnType<import("@actions/github").getOctokit>['rest']['issues']['listComments']>>['data']} params.comments
+ */
+const createCommentHandler =
+  ({ core, context, github, comments }) =>
+  async (/** @type {string[]} */ commentLines) => {
+    const owner = context.repo.owner;
+    const repo = context.repo.repo;
+    const pullNumber = context.issue.number;
+
+    const commentFound = containsAny(comments);
+
+    if (commentFound) {
+      core.info(`>>> Updating existing comment on PR`);
+      core.info(`>>> Comment id: ${commentFound.id}`);
+
+      if (commentFound.body?.startsWith(commentLines[0])) {
+        core.info(`>>> PR already has the needed comment.`);
+        core.info(`>>> Exiting gracefully! 👍`);
+        return;
+      }
+
+      return await github.rest.issues.updateComment({
+        owner,
+        repo,
+        comment_id: commentFound.id,
+        body: commentLines.join('\n\n'),
+      });
+    }
+
+    core.info(`>>> Creating explanatory comment on PR`);
+    return await github.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: pullNumber,
+      body: commentLines.join('\n\n'),
+    });
+  };
+
 /**
  * @param {Object} params
  * @param {import("@actions/core")} params.core
@@ -52,66 +113,51 @@ module.exports = async ({ core, context, github }) => {
       per_page: 100,
     });
 
-    const commentFound = prComments?.find((c) =>
-      c.body.includes(
-        'Please add one type label to categorize the purpose of this PR appropriately:',
-      ),
-    );
+    const commentHandler = createCommentHandler({
+      core,
+      context,
+      github,
+      comments: prComments,
+    });
 
-    const commentLines = [];
+    // docs label is handled differently
+    if (typeLabelsFound.some((l) => l === 'docs')) {
+      core.info(`>>> 'docs' type label found`);
+
+      await commentHandler([COMMENT_FIRST_LINE.SUCCESS_COMMENT]);
+      return;
+    }
 
     if (typeLabelsFound.length === 0) {
       core.info(`>>> No type labels found`);
 
-      if (commentFound) {
-        core.info(`>>> PR already has the type label comment.`);
-        core.info(`>>> Exiting gracefully! 👍`);
-        return;
-      }
-
       // Add a comment line explaining that a type label needs to be added
-      commentLines.push(
-        'Please add one type label to categorize the purpose of this PR appropriately:',
-      );
-      commentLines.push(createEnumerationFromArray(typeLabels));
-    } else if (typeLabelsFound.length > 1) {
-      core.info(`>>> Multiple type labels found: ${typeLabelsFound.join(', ')}`);
+      await commentHandler([
+        COMMENT_FIRST_LINE.NO_LABELS_COMMENT,
+        createEnumerationFromArray(typeLabels),
+      ]);
 
-      // add a comment line explaining that only one type label is allowed
-      commentLines.push(`Multiple type labels found: ${typeLabelsFound.join(', ')}`);
-      commentLines.push(
-        'Only one is allowed. Please remove the extra type labels to ensure the PR is categorized correctly.',
-      );
-    } else {
-      if (commentFound) {
-        core.info(`>>> PR already has the type label comment.`);
-        core.info(`>>> Trying to update the comment with id: ${commentFound.id}`);
-        await github.rest.issues.updateComment({
-          owner,
-          repo,
-          comment_id: commentFound.id,
-          body: 'Thanks for adding a type label to the PR! 👍',
-        });
-        return;
-      }
-
-      core.info(`>>> Single type label found: ${typeLabelsFound[0]}`);
-      core.info(`>>> Exiting gracefully! 👍`);
+      core.setFailed('>>> Failing workflow to prevent merge without passing this!');
       return;
     }
 
-    core.info(`>>> No type labels found.`);
-    core.info(`>>> Comment: \n"${commentLines.join('\n\n')}"`);
+    if (typeLabelsFound.length > 1) {
+      core.info(`>>> Multiple type labels found: ${typeLabelsFound.join(', ')}`);
 
-    core.info(`>>> Creating explanatory comment on PR`);
-    await github.rest.issues.createComment({
-      owner,
-      repo,
-      issue_number: pullNumber,
-      body: commentLines.join('\n\n'),
-    });
+      // add a comment line explaining that only one type label is allowed
+      await commentHandler([
+        COMMENT_FIRST_LINE.MULTIPLE_LABELS_COMMENT,
+        typeLabelsFound.map((label) => `- ${label}`).join('\n'),
+        'Only one is allowed. Please remove the extra type labels to ensure the PR is categorized correctly.',
+      ]);
 
-    core.setFailed('>>> Failing workflow to prevent merge without passing this!');
+      core.setFailed('>>> Failing workflow to prevent merge without passing this!');
+      return;
+    }
+
+    core.info(`>>> Single type label found: ${typeLabelsFound[0]}`);
+
+    await commentHandler([COMMENT_FIRST_LINE.SUCCESS_COMMENT]);
   } catch (error) {
     core.error(`>>> Workflow failed with: ${error.message}`);
     core.setFailed(error.message);
