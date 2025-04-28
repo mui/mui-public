3ce58a13e7d26677984bd92df123cf90e37647d0
Olivier Tassinari <olivier.tassinari@gmail.com>
2025-04-28 16:53:00 +0200

    fix typo
    Signed-off-by: Olivier Tassinari <olivier.tassinari@gmail.com>

--- .github/workflows/scripts/prs/checkTypeLabel.js
+++ .github/workflows/scripts/prs/checkTypeLabel.js
@@ -14,7 +14,7 @@ const typeLabels = [
   'regression', // a sub-type of bug but we flatten it.
   'enhancement',
   'new feature',
-  // Those are not "Types" labels, they are "Kind of work" labels, but we don't want to have to handle the overhead of.
+  // Those are not "Types" labels, they are "Kind of work" labels, but we don't want to have to handle the overhead of
   // setting “Types” labels with them.
   'release',
   'dependencies',
