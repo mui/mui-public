8e38a0c8aeb1927de501ddc22affcd72ad0b0406
Brijesh Bittu <brijesh42@gmail.com>
2025-06-03 12:57:28 +0530

    Sync with changes in core

--- packages/mui-infra/src/eslint/material-ui/index.mjs
+++ packages/mui-infra/src/eslint/material-ui/index.mjs
@@ -4,6 +4,7 @@ import docgenIgnoreBeforeComment from './rules/docgen-ignore-before-comment.mjs'
 import muiNameMatchesComponentName from './rules/mui-name-matches-component-name.mjs';
 import noEmptyBox from './rules/no-empty-box.mjs';
 import noHardcodedLabels from './rules/no-hardcoded-labels.mjs';
+import noRestrictedResolvedImports from './rules/no-restricted-resolved-imports.mjs';
 import noStyledBox from './rules/no-styled-box.mjs';
 import rulesOfUseThemeVariants from './rules/rules-of-use-theme-variants.mjs';
 import straightQuotes from './rules/straight-quotes.mjs';
@@ -26,5 +27,6 @@ export default {
     'no-styled-box': noStyledBox,
     'straight-quotes': straightQuotes,
     'disallow-react-api-in-server-components': disallowReactApiInServerComponents,
+    'no-restricted-resolved-imports': noRestrictedResolvedImports,
   },
 };
