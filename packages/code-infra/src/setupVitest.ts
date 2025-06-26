60b815133ab883126f2b603d5b20e98c05bed8e1
Janpot <2109932+Janpot@users.noreply.github.com>
2025-06-26 14:35:20 +0200

    Update setupVitest.ts

--- packages/code-infra/src/setupVitest.ts
+++ packages/code-infra/src/setupVitest.ts
@@ -1,14 +1,14 @@
 import { RuleTester as EslintRuleTester } from 'eslint';
-import { RuleTester as typescriptEslintRuleTester } from '@typescript-eslint/rule-tester';
+import { RuleTester as TypescriptEslintRuleTester } from '@typescript-eslint/rule-tester';
 import { describe, it, afterAll } from 'vitest';
 
 EslintRuleTester.describe = describe;
 EslintRuleTester.it = it;
 EslintRuleTester.itOnly = it.only;
 
-typescriptEslintRuleTester.afterAll = afterAll;
-typescriptEslintRuleTester.describe = describe;
-typescriptEslintRuleTester.describeSkip = describe.skip;
-typescriptEslintRuleTester.it = it;
-typescriptEslintRuleTester.itSkip = it.skip;
-typescriptEslintRuleTester.itOnly = it.only;
+TypescriptEslintRuleTester.afterAll = afterAll;
+TypescriptEslintRuleTester.describe = describe;
+TypescriptEslintRuleTester.describeSkip = describe.skip;
+TypescriptEslintRuleTester.it = it;
+TypescriptEslintRuleTester.itSkip = it.skip;
+TypescriptEslintRuleTester.itOnly = it.only;
