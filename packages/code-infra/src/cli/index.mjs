3dc2523afb5b9cc7c792bc47467565962d248d77
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2025-07-03 11:34:39 +0200

    [infra] Port argos push cli (#389)

--- packages/code-infra/src/cli/index.mjs
+++ packages/code-infra/src/cli/index.mjs
@@ -5,12 +5,14 @@ import cmdPublish from './cmdPublish.mjs';
 import cmdPublishCanary from './cmdPublishCanary.mjs';
 import cmdListWorkspaces from './cmdListWorkspaces.mjs';
 import cmdJsonLint from './cmdJsonLint.mjs';
+import cmdArgosPush from './cmdArgosPush.mjs';
 
 yargs()
   .command(cmdPublish)
   .command(cmdPublishCanary)
   .command(cmdListWorkspaces)
   .command(cmdJsonLint)
+  .command(cmdArgosPush)
   .demandCommand(1, 'You need at least one command before moving on')
   .help()
   .parse(hideBin(process.argv));
