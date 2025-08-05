ad63edcb005175f8a6087a828e52648c7b4f1635
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2025-08-05 13:54:28 +0200

    [infra] Authenticate bundle size checker GitHub API on GitHub Actions (#442)

--- packages/bundle-size-checker/src/github.js
+++ packages/bundle-size-checker/src/github.js
@@ -1,7 +1,11 @@
 // @ts-check
 
 import { Octokit } from '@octokit/rest';
+import { createActionAuth } from '@octokit/auth-action';
 
 // Create and export Octokit instance
 /** @type {import('@octokit/rest').Octokit} */
-export const octokit = new Octokit();
+export const octokit = new Octokit({
+  authStrategy: process.env.GITHUB_TOKEN ? createActionAuth : undefined,
+  userAgent: 'bundle-size-checker',
+});
