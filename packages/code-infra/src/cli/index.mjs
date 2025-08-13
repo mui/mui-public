01a4b2c7a0dbc772ca3a8d8d458fec5e301ed3d6
Brijesh Bittu <717550+brijeshb42@users.noreply.github.com>
2025-08-13 17:22:35 +0530

    [code-infra] Migrate build command (#408)

--- packages/code-infra/src/cli/index.mjs
+++ packages/code-infra/src/cli/index.mjs
@@ -1,12 +1,17 @@
+import { createRequire } from 'node:module';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
+import cmdArgosPush from './cmdArgosPush.mjs';
+import cmdBuild from './cmdBuild.mjs';
+import cmdCopyFiles from './cmdCopyFiles.mjs';
+import cmdJsonLint from './cmdJsonLint.mjs';
+import cmdListWorkspaces from './cmdListWorkspaces.mjs';
 import cmdPublish from './cmdPublish.mjs';
 import cmdPublishCanary from './cmdPublishCanary.mjs';
-import cmdListWorkspaces from './cmdListWorkspaces.mjs';
-import cmdJsonLint from './cmdJsonLint.mjs';
-import cmdArgosPush from './cmdArgosPush.mjs';
 import cmdSetVersionOverrides from './cmdSetVersionOverrides.mjs';
+
+const pkgJson = createRequire(import.meta.url)('../../package.json');
 
 yargs()
   .command(cmdPublish)
@@ -15,6 +20,9 @@ yargs()
   .command(cmdJsonLint)
   .command(cmdArgosPush)
   .command(cmdSetVersionOverrides)
+  .command(cmdCopyFiles)
+  .command(cmdBuild)
   .demandCommand(1, 'You need at least one command before moving on')
   .help()
+  .version(pkgJson.version)
   .parse(hideBin(process.argv));
