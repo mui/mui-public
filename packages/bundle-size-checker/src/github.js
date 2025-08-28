57ff0b9770c28cb85f08805dd20c0b77dd265bc5
Janpot <2109932+Janpot@users.noreply.github.com>
2025-08-28 14:58:59 +0200

    Update github.js

--- packages/bundle-size-checker/src/github.js
+++ packages/bundle-size-checker/src/github.js
@@ -1,10 +1,12 @@
 // @ts-check
 
 import { Octokit } from '@octokit/rest';
+import { createActionAuth } from '@octokit/auth-action';
 
 // Create and export Octokit instance
 /** @type {import('@octokit/rest').Octokit} */
 export const octokit = new Octokit({
-  auth: process.env.DANGER_GITHUB_API_TOKEN,
+  authStrategy: process.env.GITHUB_TOKEN ? createActionAuth : undefined,
+  auth: process.env.DANGER_GITHUB_TOKEN,
   userAgent: 'bundle-size-checker',
 });
