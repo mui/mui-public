70a30559ee20a999031b5afc389927194f279f7f
Brijesh Bittu <717550+brijeshb42@users.noreply.github.com>
2025-09-01 15:30:36 +0530

    [code-infra] Abstract tsdown based build-new command
    This is done in a way so that we could potentially remove/replace tsdown
    in future with a new bundler (if required).
    Also, this doesn't change our current `code-infra build` command with
    flags.

---
+++ packages/code-infra/src/cli/cmdBuildNew.mjs
@@ -0,0 +1,98 @@
+/* eslint-disable no-console */
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+import { markFn, measureFn, validatePkgJson } from '../utils/build.mjs';
+
+/**
+ * @typedef {Omit<import('./cmdBuild.mjs').Args, 'hasLargeFiles' | 'skipBundlePackageJson' | 'skipMainCheck' | 'cjsOutDir' | 'skipBabelRuntimeCheck' | 'skipTsc'> & {watch?: boolean; sourceMap?: boolean}} BaseArgs
+ */
+
+/**
+ * @typedef {BaseArgs & { bundler: 'tsdown' | 'rslib'}} Args
+ */
+
+/**
+ * @typedef {Partial<import('../../package.json')>} PackageJson
+ */
+
+/**
+ * @type {import('../utils/build.mjs').BundleType[]}
+ */
+const validBundles = ['esm', 'cjs'];
+
+/**
+ * @type {Args['bundler'][]}
+ */
+const validBundlers = ['tsdown'];
+
+export default /** @type {import('yargs').CommandModule<{}, Args>} */ ({
+  command: 'build-new',
+  describe: 'Builds the package for publishing.',
+  builder(yargs) {
+    return yargs
+      .option('bundler', {
+        demandOption: true,
+        type: 'string',
+        choices: validBundlers,
+        description: 'The bundler to use for building the package.',
+        default: 'tsdown',
+      })
+      .option('bundle', {
+        array: true,
+        demandOption: true,
+        type: 'string',
+        choices: validBundles,
+        description: 'Bundles to output',
+        default: ['esm', 'cjs'],
+      })
+      .option('verbose', {
+        type: 'boolean',
+        default: false,
+        description: 'Enable verbose logging.',
+      })
+      .option('buildTypes', {
+        type: 'boolean',
+        default: true,
+        description: 'Whether to build types for the package.',
+      })
+      .option('sourceMap', {
+        type: 'boolean',
+        default: false,
+        description: 'Enable source maps for the build.',
+      })
+      .option('skipPackageJson', {
+        type: 'boolean',
+        default: false,
+        description: 'Skip generating the package.json file in the bundle output.',
+      })
+      .option('watch', {
+        type: 'boolean',
+        default: false,
+        description: 'Watch files for changes and rebuild automatically.',
+      });
+  },
+  async handler({ bundler, ...args }) {
+    let pkgName = '';
+    await markFn('build-new', async () => {
+      const cwd = process.cwd();
+      const pkgJson = JSON.parse(await fs.readFile(path.join(cwd, 'package.json'), 'utf8'));
+
+      if (!bundler) {
+        throw new Error('No bundler specified');
+      }
+      pkgName = pkgJson.name;
+      console.log(`⚒️ Building ${pkgJson.name} with 📦 "${bundler}"`);
+      validatePkgJson(pkgJson);
+
+      switch (bundler) {
+        case 'tsdown':
+        default:
+          await import('../bundlers/tsdown.mjs').then(({ build }) => build(args, pkgJson));
+          break;
+      }
+    });
+    console.log(
+      `✅ Built "${pkgName}" in ${(measureFn('build-new').duration / 1000).toFixed(3)}s.`,
+    );
+  },
+});
--- packages/code-infra/src/cli/index.mjs
+++ packages/code-infra/src/cli/index.mjs
@@ -4,6 +4,7 @@ import { hideBin } from 'yargs/helpers';
 
 import cmdArgosPush from './cmdArgosPush.mjs';
 import cmdBuild from './cmdBuild.mjs';
+import cmdBuildNew from './cmdBuildNew.mjs';
 import cmdCopyFiles from './cmdCopyFiles.mjs';
 import cmdExtractErrorCodes from './cmdExtractErrorCodes.mjs';
 import cmdGithubAuth from './cmdGithubAuth.mjs';
@@ -14,24 +15,30 @@ import cmdPublishNewPackage from './cmdPublishNewPackage.mjs';
 import cmdSetVersionOverrides from './cmdSetVersionOverrides.mjs';
 import cmdValidateBuiltTypes from './cmdValidateBuiltTypes.mjs';
 
-const pkgJson = createRequire(import.meta.url)('../../package.json');
+export function run() {
+  const { version } = createRequire(import.meta.url)(
+    process.env.MUI_VERSION ? '../package.json' : '../../package.json',
+  );
 
-yargs()
-  .scriptName('code-infra')
-  .usage('$0 <command> [args]')
-  .command(cmdArgosPush)
-  .command(cmdBuild)
-  .command(cmdCopyFiles)
-  .command(cmdExtractErrorCodes)
-  .command(cmdGithubAuth)
-  .command(cmdListWorkspaces)
-  .command(cmdPublish)
-  .command(cmdPublishCanary)
-  .command(cmdPublishNewPackage)
-  .command(cmdSetVersionOverrides)
-  .command(cmdValidateBuiltTypes)
-  .demandCommand(1, 'You need at least one command before moving on')
-  .strict()
-  .help()
-  .version(pkgJson.version)
-  .parse(hideBin(process.argv));
+  yargs()
+    .scriptName('code-infra')
+    .usage('$0 <command> [args]')
+    .command(cmdArgosPush)
+    .command(cmdBuild)
+    .command(cmdBuildNew)
+    .command(cmdCopyFiles)
+    .command(cmdExtractErrorCodes)
+    .command(cmdGithubAuth)
+    .command(cmdListWorkspaces)
+    .command(cmdPublish)
+    .command(cmdPublishCanary)
+    .command(cmdPublishNewPackage)
+    .command(cmdSetVersionOverrides)
+    .command(cmdValidateBuiltTypes)
+
+    .demandCommand(1, 'You need at least one command before moving on')
+    .strict()
+    .help()
+    .version(version)
+    .parse(hideBin(process.argv));
+}
