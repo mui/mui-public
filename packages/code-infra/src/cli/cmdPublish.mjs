31ba8573c32e46002485f5ba83dc408bd724816b
Janpot <2109932+Janpot@users.noreply.github.com>
2025-09-01 14:41:00 +0200

    Update cmdPublish.mjs

--- packages/code-infra/src/cli/cmdPublish.mjs
+++ packages/code-infra/src/cli/cmdPublish.mjs
@@ -300,7 +300,6 @@ export default /** @type {import('yargs').CommandModule<{}, Args>} */ ({
         console.log('\n🚀 Would create GitHub draft release (dry-run)');
         console.log(githubReleaseData?.changelogContent);
       } else {
-        console.log(JSON.stringify(githubReleaseData, null, 2));
         await createRelease(
           githubReleaseData.version,
           githubReleaseData.changelogContent,
