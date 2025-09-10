656f7da107b4b9402a2cd0760e4ac5022fe36e97
dav-is <mail@connordav.is>
2025-09-10 17:22:05 -0400

    Start adding types.ts files

---
+++ docs/app/docs-infra/functions/hast-utils/types.ts
@@ -0,0 +1,4 @@
+import { createTypes } from '@/functions/createTypes';
+import * as hastUtils from '@mui/internal-docs-infra/pipeline/hastUtils';
+
+export const TypesHastUtils = createTypes(import.meta.url, hastUtils);
