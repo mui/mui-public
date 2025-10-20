18221c23b6f0ed9cda64261b0737f7f06216fcb0
Brijesh Bittu <717550+brijeshb42@users.noreply.github.com>
2025-10-20 14:15:08 +0530

    refactor d.ts validation
    No need to use double regex

--- packages/code-infra/src/cli/cmdValidateBuiltTypes.mjs
+++ packages/code-infra/src/cli/cmdValidateBuiltTypes.mjs
@@ -1,10 +1,11 @@
 /* eslint-disable no-console */
 import { globby } from 'globby';
 import * as fs from 'node:fs/promises';
-import * as path from 'node:path';
 import { findWorkspaceDir } from '@pnpm/find-workspace-dir';
 
 import { mapConcurrently } from '../utils/build.mjs';
+
+const DYNAMIC_PACKAGES_IMPORT_REGEX = /import\((['"])packages\//gm;
 
 /**
  * Validates if there are no missing exports from TS files that would
@@ -13,56 +14,28 @@ import { mapConcurrently } from '../utils/build.mjs';
 async function validateFiles() {
   const cwd = process.cwd();
   const workspaceRoot = await findWorkspaceDir(cwd);
-  const declarationFiles = await globby(['packages/*/build/**/*.d.{ts,cts,mts}'], {
+  const declarationFiles = await globby(['**/build/**/*.d.{ts,cts,mts}'], {
     absolute: true,
     ignore: ['node_modules'],
     followSymbolicLinks: false,
     cwd: workspaceRoot,
   });
-  /**
-   * @type {string[]}
-   */
-  const invalidFiles = [];
-  /**
-   * @type {string[]}
-   */
-  const invalidFilesWithMuiImportErrors = [];
 
-  await mapConcurrently(
-    declarationFiles,
-    async (declarationFile) => {
-      const content = await fs.readFile(declarationFile, 'utf-8');
-      const regex = /import\(["']packages\//gm;
-      if (regex.test(content)) {
-        invalidFiles.push(declarationFile);
-      }
-      const typeImportsFromMuiPackages = declarationFile.match(/import\(("|')packages\/mui/g);
+  const invalidFiles = (
+    await mapConcurrently(
+      declarationFiles,
+      async (declarationFile) => {
+        const content = await fs.readFile(declarationFile, 'utf-8');
+        const matches = Array.from(content.matchAll(DYNAMIC_PACKAGES_IMPORT_REGEX));
+        return matches.length > 0 ? declarationFile : undefined;
+      },
+      20,
+    )
+  ).filter((file) => typeof file === 'string');
 
-      if (typeImportsFromMuiPackages !== null) {
-        invalidFilesWithMuiImportErrors.push(
-          // readable path for CI while making it clickable locally
-          `${path.relative(cwd, declarationFile)} imports types ${
-            typeImportsFromMuiPackages.length
-          } times from other packages that are unreachable once published.`,
-        );
-      }
-    },
-    20,
-  );
-
-  if (invalidFiles.length) {
+  if (invalidFiles.length > 0) {
     console.error('❌ Found invalid imports in the following files:');
-    invalidFiles.forEach((file) => console.error(file));
-  }
-
-  if (invalidFilesWithMuiImportErrors.length) {
-    console.error(
-      '❌ Found invalid MUI imports in the following files (see individual errors below):',
-    );
-    invalidFilesWithMuiImportErrors.forEach((error) => console.error(error));
-  }
-
-  if (invalidFiles.length > 0 || invalidFilesWithMuiImportErrors.length > 0) {
+    console.log(invalidFiles.join('\n'));
     process.exit(1);
   }
 
