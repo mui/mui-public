0fce0d24910305928c3f2fa46047fce54243e8f7
Janpot <2109932+Janpot@users.noreply.github.com>
2026-01-07 10:20:57 +0100

    Update layout.tsx

--- apps/code-infra-dashboard/app/layout.tsx
+++ apps/code-infra-dashboard/app/layout.tsx
@@ -116,6 +116,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
         <title>Code infra dashboard</title>
       </head>
       <body>
