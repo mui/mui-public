7ce7513dbc6611c9717f742ccfc92346ead4776b
Janpot <2109932+Janpot@users.noreply.github.com>
2026-01-07 13:08:18 +0100

    bring back netlify

--- apps/code-infra-dashboard/next.config.mjs
+++ apps/code-infra-dashboard/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: process.env.NETLIFY ? 'export' : undefined,
   transpilePackages: ['@mui/internal-bundle-size-checker'],
 };
 
