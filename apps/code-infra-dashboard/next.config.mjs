ac9e83366ba93b059a60b4502f59c51007cb4bcc
Janpot <2109932+Janpot@users.noreply.github.com>
2026-01-07 14:37:48 +0100

    Revert "bring back netlify"
    This reverts commit 7ce7513dbc6611c9717f742ccfc92346ead4776b.

--- apps/code-infra-dashboard/next.config.mjs
+++ apps/code-infra-dashboard/next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.NETLIFY ? 'export' : undefined,
   transpilePackages: ['@mui/internal-bundle-size-checker'],
 };
 
