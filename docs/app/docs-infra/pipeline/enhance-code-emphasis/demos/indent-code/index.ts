cad146f1682e0cb63baeeab28454423d77ef7c89
Connor Davis <mail@connordav.is>
2026-03-09 13:55:16 -0400

    [docs-infra] Add focused code frames using `@highlight` comments (#1147)

---
+++ docs/app/docs-infra/pipeline/enhance-code-emphasis/demos/indent-code/index.ts
@@ -0,0 +1,7 @@
+import { createDemo } from '@/functions/createDemo';
+import { IndentCode } from './IndentCode';
+
+export const DemoIndentCode = createDemo(import.meta.url, IndentCode, {
+  name: 'Indent Shifting',
+  slug: 'indent-shifting',
+});
