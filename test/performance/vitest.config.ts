097c3a001fbc4b01c2dcec9ab19d306a23d98ccc
Janpot <2109932+Janpot@users.noreply.github.com>
2026-03-19 16:54:55 +0100

    Simplify

--- test/performance/vitest.config.ts
+++ test/performance/vitest.config.ts
@@ -1,4 +1,3 @@
-import { defineConfig } from 'vitest/config';
 import { createBenchmarkVitestConfig } from '@mui/internal-benchmark/vitest';
 
-export default defineConfig(() => createBenchmarkVitestConfig());
+export default createBenchmarkVitestConfig();
