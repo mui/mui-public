aa32d4b89bc8ad917a32acd2f9252526fd88ab26
Janpot <2109932+Janpot@users.noreply.github.com>
2026-03-23 13:44:16 +0100

    Update cli.js

--- packages/bundle-size-checker/src/cli.js
+++ packages/bundle-size-checker/src/cli.js
@@ -246,7 +246,11 @@ async function run(argv) {
   if (config && config.upload) {
     try {
       // eslint-disable-next-line no-console
-      console.log(`Uploading bundle size snapshot via dashboard API at ${config.upload.apiUrl}...`);
+      console.log(
+        config.upload.legacyUpload
+          ? 'Uploading bundle size snapshot directly to S3 (legacy)...'
+          : `Uploading bundle size snapshot via dashboard API at ${config.upload.apiUrl}...`,
+      );
       const { key } = await uploadSnapshot(snapshotDestPath, config.upload);
       // eslint-disable-next-line no-console
       console.log(`Bundle size snapshot uploaded to S3 with key: ${key}`);
