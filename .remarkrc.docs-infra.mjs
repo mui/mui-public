a903c2c67e05177f156d273315c4a91a294e5215
Janpot <2109932+Janpot@users.noreply.github.com>
2026-04-23 15:39:48 +0200

    fixes

--- .remarkrc.docs-infra.mjs
+++ .remarkrc.docs-infra.mjs
@@ -1,6 +1,6 @@
 import { createRemarkConfig } from '@mui/internal-code-infra/remark';
 
-// TODO @dav-is: re-enable and fix violations under docs/app/docs-infra.
 export default createRemarkConfig({
-  disable: ['no-heading-punctuation', 'no-duplicate-headings'],
+  // TODO @dav-is: re-enable and fix violations under docs/app/docs-infra.
+  disable: ['no-duplicate-headings'],
 });
