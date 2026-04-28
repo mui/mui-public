10239f493efefae7227daecc5516d64107d1a6ec
Janpot <2109932+Janpot@users.noreply.github.com>
2026-04-28 11:03:32 +0200

    Update .remarkrc.mjs

--- .remarkrc.mjs
+++ .remarkrc.mjs
@@ -6,5 +6,11 @@ export default createRemarkConfig({
       files: 'docs/app/docs-infra/pipeline/*/types.md',
       rules: { 'no-duplicate-headings': false },
     },
+    {
+      // TODO @dav-is: replace `[//]: # 'comment'` idiom with `<!-- comment -->`
+      // and drop these overrides.
+      files: 'docs/app/docs-infra/**',
+      rules: { 'no-empty-url': false, 'no-unused-definitions': false },
+    },
   ],
 });
