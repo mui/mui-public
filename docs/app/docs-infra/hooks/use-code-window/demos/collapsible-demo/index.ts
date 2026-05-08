71274322360f21d65b2cc8688378a261ab652b42
dav-is <mail@connordav.is>
2026-05-07 21:38:51 -0400

    Properly wrap useCodeWindow demo

--- docs/app/docs-infra/hooks/use-code-window/demos/collapsible-demo/index.ts
+++ docs/app/docs-infra/hooks/use-code-window/demos/collapsible-demo/index.ts
@@ -1,7 +1,7 @@
-import { createDemo } from './createDemo';
-import { Counter } from './Counter';
+import { createDemo } from '@/functions/createDemo';
+import { DemoCounter } from './demo-counter';
 
-export const DemoCollapsibleDemo = createDemo(import.meta.url, Counter, {
+export const DemoCollapsibleDemo = createDemo(import.meta.url, DemoCounter, {
   name: 'Collapsible Demo',
   slug: 'collapsible-demo',
 });
