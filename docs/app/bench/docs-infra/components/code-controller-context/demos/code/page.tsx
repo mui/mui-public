18ed66cb9ece2535a85192bdd09d7173916407dc
dav-is <mail@connordav.is>
2026-06-12 14:46:07 -0400

    add missing pages

---
+++ docs/app/bench/docs-infra/components/code-controller-context/demos/code/page.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { DemoCodeControllerContextPerformance } from '.';
+
+export default function Page() {
+  return <DemoCodeControllerContextPerformance />;
+}
---
+++ docs/app/bench/docs-infra/components/code-highlighter/demos/code/page.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { DemoCodeHighlighterPerformance } from '.';
+
+export default function Page() {
+  return <DemoCodeHighlighterPerformance />;
+}
