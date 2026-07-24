30f59ce68c60d840734f4947c33295cbd85f7c09
Brijesh Bittu <717550+brijeshb42@users.noreply.github.com>
2026-07-24 09:47:52 +0530

    Add perf measurements

--- packages/docs-infra/src/lite/loaders/demo/index.ts
+++ packages/docs-infra/src/lite/loaders/demo/index.ts
@@ -1,2 +1,7 @@
 export { loadDemo, default } from './demoLoader';
-export type { DemoEmphasisOptions, DemoLoaderContext, LoadDemoOptions } from './demoLoader';
+export type {
+  DemoEmphasisOptions,
+  DemoLoaderContext,
+  DemoPerformanceOptions,
+  LoadDemoOptions,
+} from './demoLoader';
