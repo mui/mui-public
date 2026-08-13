a4e6057aead8aeb9a8bd6dedac25019a92bb2c0e
Brijesh Bittu <717550+brijeshb42@users.noreply.github.com>
2026-08-13 16:06:42 +0530

    [docs-infra] Add shared demo external generation

---
+++ packages/docs-infra/src/pipeline/generateDemoExternalsModule/index.ts
@@ -0,0 +1,7 @@
+export * from './errors';
+export * from './generateDemoExternalsModule';
+export type * from './types';
+export { filterRuntimeExternals } from '../loadPrecomputedCodeHighlighterClient/filterRuntimeExternals';
+export { findServerOnlyExternals } from '../loadPrecomputedCodeHighlighterClient/findServerOnlyExternals';
+export { generateResolvedExternals } from '../loadPrecomputedCodeHighlighterClient/generateResolvedExternals';
+export { injectImportsIntoSource } from '../loadPrecomputedCodeHighlighterClient/injectImportsIntoSource';
